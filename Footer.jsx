fb66888230ae523e4c722da022b625e09f6727a3
akash18032001 <84910398+akash18032001@users.noreply.github.com>
2021-06-27 20:19:45 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyrigth By ShapeAI @{new Date().getFullYear()}</p>
+      </footer>
+      
+  );
+}
+
+export default Footer;
